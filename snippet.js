b805e79713054823fddac5a70ef7cd2e792c6e4a
Neil Vinod <neilvinod14@gmail.com>
2018-08-26 21:09:05 -0700

    0x removal:

--- snippet.js
+++ snippet.js
@@ -1,4 +1,8 @@
-signature = signature.substr(2); //remove 0x
+//purpose is to remove 0x
+
+signature = signature.substr(2);
+
+
 const r = '0x' + signature.slice(0, 64)
 const s = '0x' + signature.slice(64, 128)
 const v = '0x' + signature.slice(128, 130)
